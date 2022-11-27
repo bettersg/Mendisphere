81f0af9c510035de8ea798b56b7a95db2cfc7bbb
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2022-11-27 16:48:21 +0800

    MBF-00008 added a component for adding images or icons in the login component

---
+++ src/features/login/components/login-design.tsx
@@ -0,0 +1,31 @@
+import { Box, Button, Center, Container, Flex, Link, Spacer, StackDivider, Text, VStack, Image } from '@chakra-ui/react'
+
+
+
+
+
+export default function LoginDesign(){
+    return (
+        
+        <VStack>
+            <Spacer />
+            <Spacer />
+            <Center minH="10em" >
+                
+            </Center>
+           
+            <Center minH="10em" >
+                <Image src="/images/casual-life-3d-female-sitting-on-the-floor-with-cup-in-hand-and-laptop-on-leg.png"></Image>
+            </Center>
+
+
+            
+
+        </VStack>
+
+
+
+    )
+
+
+}
