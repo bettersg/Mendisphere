2571373c1d59cce129c8f5e519edc251fcfefb97
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2022-11-27 16:19:54 +0800

    MBF-00008 added show/hide password button in the login component

--- src/features/login/components/email-credential.tsx
+++ src/features/login/components/email-credential.tsx
@@ -1,7 +1,26 @@
-import { Box, Container, Flex, Input, StackDivider, Text, VStack } from '@chakra-ui/react'
+import { Box, Button, Container, Flex, Input, InputGroup, InputRightElement, StackDivider, Text, VStack } from '@chakra-ui/react'
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
+import React, {useState} from 'react'
+
+
 
 
 export default function EmailCredential(){
+
+    const [show, setShow] = React.useState(false)
+    const [noText, setNoText] = React.useState(true)
+
+    const handleClick = () => setShow(!show)
+    
+
+    const handleChange = (event:any) => {
+
+        if(event.target.value.length > 0){
+            setNoText(false);
+        }else{
+            setNoText(true);
+        }
+    }
 
     return (
         <Flex >
@@ -16,7 +35,21 @@ export default function EmailCredential(){
                 </Box>
                 <Box >
                     <Text>Password</Text>
-                    <Input placeholder='********' />
+                    <InputGroup size='md'>
+                        <Input
+                            pr='4.5rem'
+                            type={show ? 'text' : 'password'}
+                            placeholder='Enter password'
+                            onChange={handleChange}
+               
+                        ></Input>
+
+                        <InputRightElement width='4.5rem'>
+                            <Button h='1.75rem' size='sm' onClick={handleClick} disabled={noText}>
+                            {show ? <ViewOffIcon/> : <ViewIcon/>}
+                            </Button>
+                        </InputRightElement>
+                    </InputGroup>
                 </Box>
 
             </VStack>
