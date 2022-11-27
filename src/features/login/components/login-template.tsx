8d0c7c5ca62e035bb70c85c65b850c12101d2e7e
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2022-11-27 15:19:34 +0800

    MBF-00008 added temporary login template component

---
+++ src/features/login/components/login-template.tsx
@@ -0,0 +1,79 @@
+import { Box, Button, Container, Flex, Link, Spacer, StackDivider, Text, VStack } from '@chakra-ui/react'
+import EmailCredential from "./email-credential"
+import {ArrowBackIcon} from '@chakra-ui/icons'
+
+
+import LoginMenuMessage from './login-menu-message'
+import "./../../../assets/login/login.scss"
+
+export default function Login(){
+
+    return (
+        <Flex minH="100vH">
+            <Box flex='1'>
+                <VStack 
+                     divider={<StackDivider borderColor='#FFFFFF' />}
+                     spacing={4}
+                     align="stretch"
+                >
+                    <Container maxW='container.sm' >
+                         
+                    </Container>
+
+                    <Container maxW='container.sm' >
+                        <Link href="/"><Text><ArrowBackIcon></ArrowBackIcon>Back</Text></Link>
+                    </Container>
+
+
+                    <Container minH="10em" maxW='container.sm' >
+                      
+                    </Container>
+                    
+                    <Container minH="3em" maxW='container.sm' >
+                        <LoginMenuMessage />
+                    </Container>
+                    
+                    <Container maxW='container.sm' >
+                        <EmailCredential />
+                    </Container>
+
+                    <Container maxW='container.sm' >
+                        <Flex >
+                            <Box p='1'></Box>
+                            <Spacer />
+                            <Box p='1' >
+                                <Link href="/"><Text color="blue" as='b'>Forgot password</Text></Link>
+                            </Box>
+                        </Flex>
+                        <VStack
+                            spacing={4}
+                            align='stretch'
+                            minWidth="100%"
+                        >
+                                <Button colorScheme='blue'>Button</Button>
+                        </VStack>
+                        <Flex >
+                            <Spacer />
+                            <Box p="1" >
+                                <Text color="grey" as='b'>Don't Have an account? </Text>
+                            </Box>
+                            <Box p="1">
+                                <Link href="/"><Text color="blue" as='b'>Sign up</Text></Link>
+                            </Box>
+                            <Spacer />
+                        </Flex>
+                    </Container>
+
+                </VStack>
+            </Box>
+            <Box flex='1' className="rounded_edge_rectangle">
+                put image here
+            </Box>
+                
+      
+        </Flex>
+
+    )
+
+}
+
