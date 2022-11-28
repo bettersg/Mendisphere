0481c694fa430148b59f89e07d1cf021031a7a05
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2022-11-28 13:59:22 +0800

    MBF-00008 refactor the components in the left section of login feature into different components

---
+++ src/features/login/components/login-section.tsx
@@ -0,0 +1,39 @@
+import { ArrowBackIcon } from '@chakra-ui/icons'
+import { Box, Button, Container, Flex, Link, Spacer, StackDivider, Text, VStack } from '@chakra-ui/react'
+import LoginForm from './login-form'
+import LoginMenuMessage from './login-menu-message'
+
+
+
+export default function LoginSection(){
+
+    return (
+        <VStack 
+        divider={<StackDivider borderColor='#FFFFFF' />}
+         spacing={4}
+         align="stretch"
+        >
+            <Container maxW='container.sm' />
+                
+            <Container maxW='container.sm' >
+                <Link href="/"><Text><ArrowBackIcon></ArrowBackIcon>Back</Text></Link>
+            </Container>
+
+            <Container minH="10em" maxW='container.sm' />
+            
+            <Container minH="3em" maxW='container.sm' >
+                <LoginMenuMessage />
+            </Container>
+            
+            <Container maxW='container.sm' >
+                <LoginForm />
+            </Container>
+
+       
+
+        </VStack>
+
+    )
+
+    
+}
