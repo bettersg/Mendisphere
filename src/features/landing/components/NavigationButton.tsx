027aecdc40a592de6cada5e2dfe79297bb9eb86f
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2022-12-07 13:41:50 +0800

    MBF-00011 added NavigationButton component

---
+++ src/features/landing/components/NavigationButton.tsx
@@ -0,0 +1,39 @@
+import { Box} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+export default function NavigationButton(props:any){
+
+    return (
+        <Link to={props.navigationLink}>
+            <Box
+            as='button'
+            fontFamily={"Inter"}
+            height='6vh'
+            width="9vw"
+            lineHeight='1.2'
+            transition='all 0.2s cubic-bezier(.08,.52,.52,1)'
+            border='1px'
+            px='8px'
+            borderRadius='2px'
+            fontSize='12px'
+            fontWeight='semibold'
+            bg='#192873'
+            borderColor='#ccd0d5'
+            color='#ffffff'
+            _hover={{ bg: '#ebedf0' }}
+            _active={{
+                bg: '#dddfe2',
+                transform: 'scale(0.98)',
+                borderColor: '#bec3c9',
+            }}
+            _focus={{
+                boxShadow:
+                '0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)',
+            }}
+            >
+                {props.buttonText}
+            </Box>
+        </Link>
+    )
+
+}
