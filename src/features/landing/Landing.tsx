703b25cce4130ba7f223ef89ef23e0d527f2512b
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2022-12-15 22:02:23 +0800

    MBF-00013 adjust the size of each section in the Landing page and added Footer component

--- src/features/landing/Landing.tsx
+++ src/features/landing/Landing.tsx
@@ -1,4 +1,5 @@
-import {Box, Flex, StackDivider, VStack} from '@chakra-ui/react'
+import {Box, VStack} from '@chakra-ui/react'
+import Footer from './components/Footer';
 import NavigationBar from './components/NavigationBar';
 
 export default function Landing(){
@@ -14,20 +15,20 @@ export default function Landing(){
             <Box h='87.5vh' bg='blue'>
                 Expand your impact with Mindbetter
             </Box>
-            <Box h='100vh' bg='pink.100'>
+            <Box h='81.3vh' bg='pink.100'>
                 Get connected to what matters most.
             </Box>
-            <Box h='100vh' bg='pink.100'>
+            <Box h='92.8vh' bg='pink.100'>
                 Hear their stories!
             </Box>
-            <Box h='100vh' bg='green.100'>
+            <Box h='81.6vh' bg='green.100'>
                 How does MindBetter work?
             </Box>
-            <Box h='67vh' bg='blue.100'>
+            <Box h='54.7vh' bg='blue.100'>
                 Go faster, together
             </Box>
-            <Box h='29.6vh' bg='pink.100'>
-                Footer
+            <Box h='24.1vh'>
+                <Footer />
             </Box>
         </VStack>
     );
