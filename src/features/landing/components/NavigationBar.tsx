3d0f2d504adb95db20113615d1fffd357ca7754c
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2022-12-16 10:06:41 +0800

    MBF-00023 modify the sizing of the navigation bar components to remove whitespace on the right side of the screen

--- src/features/landing/components/NavigationBar.tsx
+++ src/features/landing/components/NavigationBar.tsx
@@ -11,14 +11,14 @@ export default function NavigationBar(){
         >
             <Center h='3vh'/>
             <Center h='6vh' >
-                <Flex minWidth="max-content">
-                        <Center  w="8.8vw" h="6vh" />
+                <Flex>
+                    <Center w="8.4vw" h="6vh" />
 
                     <Center p='4' w="9vw" h="6vh" >
                         Logo Here
                     </Center>
 
-                        <Center w="14.9vw" h="6vh" />
+                    <Center w="14vw" h="6vh" />
 
                     <Center w="30vw" h="6vh">
                         <Flex>
@@ -51,9 +51,9 @@ export default function NavigationBar(){
         
                     </Center>
 
-                    <Center w="14.9vw" h="6vh"/>
+                    <Center w="14vw" h="6vh"/>
 
-                    <Center w="19.3vw" h="6vh">
+                    <Center w="15vw" h="6vh">
 
                             <NavigationButton navigationLink="/login" buttonText="Log in"/>
 
@@ -63,7 +63,7 @@ export default function NavigationBar(){
                         
                     </Center>
 
-                    <Center w="8.8vw" h="6vh" />
+                    <Center w="8.4vw" h="6vh" />
                 </Flex>
             </Center>
             <Center h='3vh' />
