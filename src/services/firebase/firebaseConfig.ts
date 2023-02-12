bd6de4c7d3998aaa053f89d81f69151baab5baae
karthik-Jay <karthik.jay@outlook.com>
2023-02-12 19:58:58 +0800

    Update firebase config file
    Removed secrets from source control.

--- src/services/firebase/firebaseConfig.ts
+++ src/services/firebase/firebaseConfig.ts
@@ -1,16 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDzOfSyn6c3BDAO3ZXFUM4KjRL_3TSDTxo",
-    authDomain: "mindbetter-bettersg.firebaseapp.com",
-    projectId: "mindbetter-bettersg",
-    storageBucket: "mindbetter-bettersg.appspot.com",
-    messagingSenderId: "963292055223",
-    appId: "1:963292055223:web:c8b64fbe722ca6436a2f32",
-    measurementId: "G-80GB9ZHTHK"
-}; 
+    apiKey: process.env.REACT_APP_MINDBETTER_APIKEY,
+    authDomain: process.env.REACT_APP_MINDBETTER_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_MINDBETTER_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_MINDBETTER_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MINDBETTER_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_MINDBETTER_APP_ID,
+    measurementId: process.env.REACT_APP_MINDBETTER_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 
+export const db = getFirestore(app);
 export const auth = getAuth(app);
