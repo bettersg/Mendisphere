33eb512f01673b2426092c1d3f9e14457a5601a0
chuazhihong <chuazhihong@hotmail.com>
2023-02-18 13:47:24 +0800

    Add homepage components with scroll animation

---
+++ src/features/home/components/ScrollAnimation.tsx
@@ -0,0 +1,81 @@
+// credits: https://betterprogramming.pub/simple-react-scroll-animations-with-zero-dependencies-b496c1e1c7bd
+import { useState, useEffect, useRef } from "react";
+
+const useElementOnScreen = (ref: React.RefObject<Element>, rootMargin = "0px") => {
+  const [isIntersecting, setIsIntersecting] = useState(true);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsIntersecting(entry.isIntersecting);
+      },
+      { rootMargin }
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+  return isIntersecting;
+}
+
+const AnimateIn: React.FC<
+React.PropsWithChildren<{ from: React.CSSProperties; to: React.CSSProperties }>
+> = ({ from, to, children }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const onScreen = useElementOnScreen(ref);
+  const defaultStyles: React.CSSProperties = {
+    transition: "600ms ease-in-out"
+  };
+  return (
+    <div
+      ref={ref}
+      style={
+        onScreen
+          ? {
+              ...defaultStyles,
+              ...to
+            }
+          : {
+              ...defaultStyles,
+              ...from
+            }
+      }
+    >
+      {children}
+    </div>
+  );
+};
+
+const FadeUp: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <AnimateIn
+    from={{ opacity: 0, translate: "0 2rem" }}
+    to={{ opacity: 1, translate: "none" }}
+  >
+    {children}
+  </AnimateIn>
+);
+
+const FadeDown: React.FC<React.PropsWithChildren> = ({ children }) => (
+  <AnimateIn
+    from={{ opacity: 0, translate: "none" }}
+    to={{ opacity: 1, translate: "0 2rem" }}
+  >
+    {children}
+  </AnimateIn>
+);
+
+const FadeIn: React.FC<React.PropsWithChildren> = ({ children }) => (
+    <AnimateIn from={{ opacity: 0 }} to={{ opacity: 1 }}>
+      {children}
+    </AnimateIn>
+  );
+
+export const Animate = {
+  FadeUp,
+  FadeDown,
+  FadeIn
+};
