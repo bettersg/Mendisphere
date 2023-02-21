d149dff8f36bec18768736136f7e46f6923dfdf4
Zhi Hong <chuazhihong@hotmail.com>
2023-02-21 20:20:48 +0800

    MBF-75 Organisation Profile Tabs UI and Firestore Data Collection

--- src/services/firebase/names.ts
+++ src/services/firebase/names.ts
@@ -3,4 +3,7 @@ export class Collections {
   static readonly organisations = "organisations";
   static readonly users = "users";
   static readonly organisationsAdminData = "organisationAdminData";
+  static readonly organisationPeopleSpotlight = "organisationPeopleSpotlight";
+  static readonly organisationFAQ = "organisationFAQ";
+  static readonly organisationOurStory = "organisationOurStory";
 }
