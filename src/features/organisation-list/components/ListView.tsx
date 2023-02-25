0a26805183c5109c2950631353a7d131d6ed44e8
karthik-Jay <karthik.jay@outlook.com>
2023-02-25 09:55:11 +0800

    Replace mock organisation definition with organisation class

--- src/features/organisation-list/components/ListView.tsx
+++ src/features/organisation-list/components/ListView.tsx
@@ -1,63 +1,81 @@
-import { useState } from 'react';
-import { Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
-import { IOrganization } from "..";
+import { useState } from "react";
+import {
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
-import { ReactComponent as Approved } from '../../../assets/icons/statusApproved.svg'
-import { ReactComponent as NotApproved } from '../../../assets/icons/statusNotApproved.svg'
-import { ReactComponent as Pending } from '../../../assets/icons/statusPending.svg'
-import { ReactComponent as Verified } from '../../../assets/icons/statusVerified.svg'
-import { ReactComponent as NotVerified } from '../../../assets/icons/statusNotVerified.svg'
+import { ReactComponent as Approved } from "../../../assets/icons/statusApproved.svg";
+import { ReactComponent as NotApproved } from "../../../assets/icons/statusNotApproved.svg";
+import { ReactComponent as Pending } from "../../../assets/icons/statusPending.svg";
+import { ReactComponent as Verified } from "../../../assets/icons/statusVerified.svg";
+import { ReactComponent as NotVerified } from "../../../assets/icons/statusNotVerified.svg";
+import { Organisation } from "../../../data/model/organisation";
+import { VerificationStatus } from "../../../data/enums/verification-status.enum";
+import { IPCStatus } from "../../../data/enums/ipc-status.enum";
 
 const ipcUIMap = {
-    ['yes' as string]: <Approved/>,
-    ['no' as string]: <NotApproved/>,
-    ['pending' as string]: <Pending/>
-}
+  [IPCStatus.Approved]: <Approved />,
+  [IPCStatus.NotApproved]: <NotApproved />,
+  [IPCStatus.Pending]: <Pending />,
+};
 
 const verifiedUIMap = {
-    ['yes' as string]: <Verified/>,
-    ['no' as string]: <NotVerified/>,
-    ['pending' as string]: <Pending/>
-}
+  [VerificationStatus.Verified]: <Verified />,
+  [VerificationStatus.NotVerified]: <NotVerified />,
+  [VerificationStatus.Pending]: <Pending />,
+};
 
-const ListView: React.FC<{organizationList: IOrganization[]}> = ({organizationList}) => {
-    const navigate = useNavigate();
-    const [bgColor, setBgColor] = useState<string>();
+const ListView: React.FC<{ organisationList: Organisation[] }> = ({
+  organisationList,
+}) => {
+  const navigate = useNavigate();
+  const [bgColor, setBgColor] = useState<string>();
 
-    return (
-        <TableContainer>
-            <Table variant='simple'>
-                {/* <TableCaption>Imperial to metric conversion factors</TableCaption> */}
-                <Thead>
-                    <Tr>
-                        <Th>ORGANISATION</Th>
-                        <Th>DESCRIPTION</Th>
-                        <Th>VERIFIED?</Th>
-                        <Th>FOCUSES ON</Th>
-                        <Th>LOOKING FOR</Th>
-                        <Th>IPC STATUS</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {organizationList.map((organization, index) => {
-                        return (
-                            <Tr
-                                onClick={() => navigate(`/organisations/${organization.index}`)}
-                                style={{cursor: 'pointer'}}
-                            >
-                                <Td>{index} name</Td>
-                                <Td>{organization.renderText}</Td>
-                                <Td>{!!organization.verified && verifiedUIMap[organization.verified]}</Td>
-                                <Td>{organization.focusesOn}</Td>
-                                <Td>{organization.lookingFor}</Td>
-                                <Td>{!!organization.IPCRegistered && ipcUIMap[organization.IPCRegistered]}</Td>
-                            </Tr>
-                        )
-                    })}
-                </Tbody>
-            </Table>
-        </TableContainer>
-    )
-}
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        {/* <TableCaption>Imperial to metric conversion factors</TableCaption> */}
+        <Thead>
+          <Tr>
+            <Th>ORGANISATION</Th>
+            <Th>DESCRIPTION</Th>
+            <Th>VERIFIED?</Th>
+            <Th>FOCUSES ON</Th>
+            <Th>LOOKING FOR</Th>
+            <Th>IPC STATUS</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {organisationList.map((organisation, index) => {
+            return (
+              <Tr
+                onClick={() => navigate(`/organisations/${organisation.id}`)}
+                style={{ cursor: "pointer" }}
+              >
+                <Td>{index} name</Td>
+                <Td>{organisation.description}</Td>
+                <Td>
+                  {!!organisation.verified &&
+                    verifiedUIMap[organisation.verified]}
+                </Td>
+                <Td>{organisation.mainSpecialisation}</Td>
+                <Td>{organisation.mainSupportArea}</Td>
+                <Td>
+                  {!!organisation.ipcApproved &&
+                    ipcUIMap[organisation.ipcApproved]}
+                </Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 export default ListView;
