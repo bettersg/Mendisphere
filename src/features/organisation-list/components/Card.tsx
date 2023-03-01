bd52850dc62814a02881ca198e1b372a14c902b8
karthik-Jay <karthik.jay@outlook.com>
2023-03-01 22:50:48 +0800

    Add text descriptions to card component

--- src/features/organisation-list/components/Card.tsx
+++ src/features/organisation-list/components/Card.tsx
@@ -1,5 +1,7 @@
 import { Organisation } from "../../../data/model/organisation";
-import { Box, Text, Image, VStack } from "@chakra-ui/react";
+import { Box, Text, VStack, HStack } from "@chakra-ui/react";
+import { ReactComponent as Verified } from "../../../assets/icons/checkMarkVerified.svg";
+import { VerificationStatus } from "../../../data/enums/verification-status.enum";
 
 const Card: React.FC<{ org: Organisation }> = ({ org }) => {
   const imageUrl = `${org.cardImageUrl}/?random&t=${new Date().getTime()}`;
@@ -12,9 +14,20 @@ const Card: React.FC<{ org: Organisation }> = ({ org }) => {
       borderRadius="12px"
       bgImage={imageUrl}
     >
-      <VStack pos="absolute" top="262px" mr="25px" ml="25px">
-        <Text fontSize="16px" fontWeight="700" color="white">
-          {org.name}
+      <VStack pos="absolute" top="262px" mr="25px" ml="25px" spacing="10px">
+        <HStack>
+          <Text fontSize="16px" fontWeight="700" color="white">
+            {org.name}
+          </Text>
+          {org.verified === VerificationStatus.Verified ? <Verified /> : <></>}
+        </HStack>
+        <HStack>
+          <Text fontSize="12px" fontWeight="400" color="white">
+            {org.mainSpecialisation}
+          </Text>
+        </HStack>
+        <Text fontSize="12px" fontWeight="200" color="white">
+          {org.description}
         </Text>
       </VStack>
     </Box>
