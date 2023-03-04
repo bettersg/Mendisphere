eeca798434504ad0a5478df93e46522fb232afa7
karthik-Jay <karthik.jay@outlook.com>
2023-03-04 14:58:11 +0800

    Update navbar to use view port dimensions

--- src/features/common/simple-header/simple-navbar.tsx
+++ src/features/common/simple-header/simple-navbar.tsx
@@ -1,10 +1,11 @@
-import { Flex, Text } from "@chakra-ui/react";
-import "./simple-navbar.scss";
+import { Box, Text } from "@chakra-ui/react";
 
 export default function SimpleNavigationBar() {
   return (
-    <Flex className="simpleNavbar">
-      <Text className="navbarText">Mindbetter</Text>
-    </Flex>
+    <Box h="8vh" borderBottom="1px" borderColor="#d3d3d3">
+      <Text fontWeight="700" paddingLeft={8} paddingTop={5} fontSize={24}>
+        Mindbetter
+      </Text>
+    </Box>
   );
 }
