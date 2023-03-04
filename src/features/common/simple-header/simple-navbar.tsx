5b96e46b1f87a71c19f3a7fdb381d72d782fa862
karthik-Jay <karthik.jay@outlook.com>
2023-03-04 15:37:32 +0800

    Update nav bar style

--- src/features/common/simple-header/simple-navbar.tsx
+++ src/features/common/simple-header/simple-navbar.tsx
@@ -1,11 +1,11 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 export default function SimpleNavigationBar() {
   return (
-    <Box h="8vh" borderBottom="1px" borderColor="#d3d3d3">
-      <Text fontWeight="700" paddingLeft={8} paddingTop={5} fontSize={24}>
-        Mindbetter
+    <Flex h="8vh" borderBottom="1px" borderColor="#d3d3d3">
+      <Text alignSelf="center" fontWeight="700" paddingLeft={8} fontSize={24}>
+        Mendisphere
       </Text>
-    </Box>
+    </Flex>
   );
 }
