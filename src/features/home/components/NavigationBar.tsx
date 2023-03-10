4cc404e3b57d920bd59f2139d3ea4ae7e80b9297
Audrey Kon <audz@kon.sg>
2023-03-10 23:32:25 +0800

    navbar responsove and closes into hamburger icon

--- src/features/home/components/NavigationBar.tsx
+++ src/features/home/components/NavigationBar.tsx
@@ -1,57 +1,67 @@
-import { Flex, VStack, StackDivider, Link, Center } from "@chakra-ui/react";
-import NavigationButton from "./NavigationButton";
+import { useState } from "react";
+import {
+  Box,
+  Flex,
+  HStack,
+  IconButton,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  Link
+} from "@chakra-ui/react";
+import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
 import { Link as ReactRouterLink } from "react-router-dom";
+import NavigationButton from "./NavigationButton";
 
-export default function NavigationBar() {
+const Navigation = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [mobileNav, setMobileNav] = useState(false);
+
+  const toggleMobileNav = () => {
+    setMobileNav(!mobileNav);
+  };
+
   return (
-    <VStack
-      divider={<StackDivider borderColor="whiteAlpha.200" />}
-      spacing={0}
-      w="98vw"
-      h="10vh"
-    >
-      <Center h="2.5vh" />
-
-      <Center h="5vh">
-        <Flex>
-          <Center w="8.8vw" h="5vh" />
-
-          <Center p="4" w="9vw" h="5vh">
-            Logo Here
-          </Center>
-
-          <Center w="14.9vw" h="5vh" />
-          {/* TODO: Create a method to calculate relative value for width(vw) and height (vh)  */}
-          <Center w="24vw" h="5vh">
-            <Flex>
-              <Center w="8vw" h="5vh">
-                <Link
-                  as={ReactRouterLink}
-                  to={"/organisations"}
-                  fontFamily={"Inter"}
-                  fontSize="12px"
-                >
-                  Organisations
-                </Link>
-              </Center>
-
-              <Center w="8vw" h="5vh">
-                <Link fontFamily={"Inter"} fontSize="12px">
-                  Apply for Services
-                </Link>
-              </Center>
-
-              <Center w="8vw" h="5vh">
-                <Link fontFamily={"Inter"} fontSize="12px" lineHeight="1.2">
-                  Contact Us
-                </Link>
-              </Center>
-            </Flex>
-          </Center>
-
-          <Center w="14.9vw" h="5vh" />
-
-          <Center w="19.3vw" h="5vh">
+    <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+      <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+        <Box>
+          Logo
+        </Box>
+        <HStack spacing={8} alignItems={"center"}>
+          <HStack
+            as={"nav"}
+            spacing={4}
+            display={{ base: "none", md: "flex" }}
+          >
+            <Link
+              as={ReactRouterLink}
+              to={"/organisations"}
+              fontFamily={"Inter"}
+            >
+              Organisations
+            </Link>
+            <Link
+              as={ReactRouterLink}
+              to={"/applyforservices"}
+              fontFamily={"Inter"}
+            >
+              Apply for Services
+            </Link>
+            <Link
+              as={ReactRouterLink}
+              to={"/contactus"}
+              fontFamily={"Inter"}
+            >
+              Contact Us
+            </Link>
+          </HStack>
+          <Flex flex={1} justify={"flex-end"}>
+            <HStack
+              direction={"row"}
+              spacing={4}
+              alignItems={"center"}
+              display={{ base: "none", md: "flex" }}
+            >
             <NavigationButton
               backgroundColor="#192873"
               navigationLink="/login"
@@ -59,9 +69,6 @@ export default function NavigationBar() {
               height="6vh"
               width="9vw"
             />
-
-            <Center w="1.2vw" h="5vh" />
-
             <NavigationButton
               backgroundColor="#192873"
               navigationLink=""
@@ -69,12 +76,63 @@ export default function NavigationBar() {
               height="6vh"
               width="9vw"
             />
-          </Center>
+            </HStack>
+              <IconButton
+                size={"md"}
+                icon={mobileNav ? <CloseIcon /> : <HamburgerIcon />}
+                aria-label={"Open Menu"}
+                display={{ md: "none" }}
+                onClick={toggleMobileNav}
+              />
+          </Flex>
+        </HStack>
+      </Flex>
 
-          <Center w="8.8vw" h="5vh" />
-        </Flex>
-      </Center>
-      <Center h="2.5vh" />
-    </VStack>
+      {mobileNav ? (
+        <Box pb={4} display={{ md: "none" }}>
+          <Stack as={"nav"} spacing={4}>
+            <Link
+              as={ReactRouterLink}
+              to={"/organisations"}
+              fontFamily={"Inter"}
+            >
+              Organisations
+            </Link>
+            <Link
+              as={ReactRouterLink}
+              to={"/applyforservices"}
+              fontFamily={"Inter"}
+            >
+              Apply for Services
+            </Link>
+            <Link
+              as={ReactRouterLink}
+              to={"/contactus"}
+              fontFamily={"Inter"}
+            >
+              Contact Us
+            </Link>
+            <Stack direction={"row"} spacing={4} alignItems={"center"}>
+              <NavigationButton
+                backgroundColor="#192873"
+                navigationLink="/login"
+                buttonText="Log in"
+                height="6vh"
+                width="9vw"
+              />
+              <NavigationButton
+                backgroundColor="#192873"
+                navigationLink=""
+                buttonText="Sign up"
+                height="6vh"
+                width="9vw"
+              />
+            </Stack>
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
   );
-}
+};
+
+export default Navigation;
