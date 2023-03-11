8b579336c3b40d4285b145c55b716301dbb621b4
karthik-Jay <karthik.jay@outlook.com>
2023-03-11 15:46:24 +0800

    Linters code format update and change font colour on hover

--- src/features/home/components/NavigationButton.tsx
+++ src/features/home/components/NavigationButton.tsx
@@ -1,39 +1,40 @@
-import { Box} from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
-export default function NavigationButton(props:any){
-
-    return (
-        <Link to={props.navigationLink}>
-            <Box
-            as='button'
-            fontFamily={"Inter"}
-            height={props.height}
-            width={props.width}
-            lineHeight='1.2'
-            transition='all 0.2s cubic-bezier(.08,.52,.52,1)'
-            border='1px'
-            px='8px'
-            borderRadius='2px'
-            fontSize='12px'
-            fontWeight='semibold'
-            bg={props.backgroundColor}
-            borderColor='#ccd0d5'
-            color='#ffffff'
-            _hover={{ bg: '#ebedf0' }}
-            _active={{
-                bg: '#dddfe2',
-                transform: 'scale(0.98)',
-                borderColor: '#bec3c9',
-            }}
-            _focus={{
-                boxShadow:
-                '0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)',
-            }}
-            >
-                {props.buttonText}
-            </Box>
-        </Link>
-    )
-
+export default function NavigationButton(props: any) {
+  return (
+    <Link to={props.navigationLink}>
+      <Box
+        as="button"
+        fontFamily={"Inter"}
+        height={props.height}
+        width={props.width}
+        lineHeight="1.2"
+        transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+        border="1px"
+        px="8px"
+        borderRadius="4px"
+        fontSize="12px"
+        fontWeight="700"
+        bg={props.backgroundColor}
+        borderColor="#ccd0d5"
+        color="#ffffff"
+        _hover={{
+          bg: "#ebedf0",
+          color: "black",
+        }}
+        _active={{
+          bg: "#dddfe2",
+          transform: "scale(0.98)",
+          borderColor: "#bec3c9",
+        }}
+        _focus={{
+          boxShadow:
+            "0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)",
+        }}
+      >
+        {props.buttonText}
+      </Box>
+    </Link>
+  );
 }
