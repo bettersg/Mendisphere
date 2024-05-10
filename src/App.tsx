414295966b6043f4fe27d23660ac8fbb0b4b047c
karthik-Jay <32955712+karthik-Jay@users.noreply.github.com>
2024-05-10 18:59:56 +0800

    Update to use theme folder structure. (#161)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import { ChakraProvider, Container } from "@chakra-ui/react";
-import { AuthProvider } from "./services/Firebase/AuthProvider";
-import { appTheme } from "./theme";
+import { ChakraProvider } from "@chakra-ui/react";
+import { AuthProvider } from "./services/firebase/authProvider";
 import Routing from "./routing";
 import { Paths } from "./routing/Paths";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import mendisphereTheme from "./theme/index";
 
 function App() {
   // Get current page
   const currentPage = window.location.pathname;
 
   // Check the current page is not login and registration page
-  const isShowHeaderAndFooter = ![Paths.login, Paths.signup, Paths.profileSetup].includes(
-    currentPage
-  );
+  const isShowHeaderAndFooter = ![
+    Paths.login,
+    Paths.signup,
+    Paths.profileSetup,
+  ].includes(currentPage);
 
   return (
-    <ChakraProvider theme={appTheme}>
+    <ChakraProvider theme={mendisphereTheme}>
       <AuthProvider>
         {isShowHeaderAndFooter && <Header />}
         <Routing />
---
+++ src/theme/typography.ts
@@ -0,0 +1,125 @@
+export const fonts = {
+  body: "Inter, sans-serif",
+  heading: "Inter, sans-serif",
+  mono: "Menlo, monospace",
+};
+
+const normalFontWeight = "400";
+const boldFontWeight = "700";
+
+const title = {
+  xxl: "2.5rem",
+  xl: "2rem",
+  lg: "1.5rem",
+  md: "1.125rem",
+  sm: "1rem",
+};
+
+export const textStyles = {
+  bg: {
+    lg: {
+      fontSize: "10rem",
+      fontWeight: boldFontWeight,
+    },
+    md: {
+      fontSize: "7.5rem",
+      fontWeight: boldFontWeight,
+    },
+    sm: {
+      fontSize: "5rem",
+      fontWeight: boldFontWeight,
+    },
+    xs: {
+      fontSize: "3.75rem",
+      fontWeight: boldFontWeight,
+    },
+  },
+
+  title: {
+    xxl: {
+      fontSize: title.xxl,
+      fontWeight: normalFontWeight,
+    },
+    "xxl-bold": {
+      fontSize: title.xxl,
+      fontWeight: boldFontWeight,
+    },
+    xl: {
+      fontSize: title.xl,
+      fontWeight: normalFontWeight,
+    },
+    "xl-bold": {
+      fontSize: title.xl,
+      fontWeight: boldFontWeight,
+    },
+    lg: {
+      fontSize: title.lg,
+      fontWeight: normalFontWeight,
+    },
+    "lg-bold": {
+      fontSize: title.lg,
+      fontWeight: boldFontWeight,
+    },
+    md: {
+      fontSize: title.md,
+      fontWeight: normalFontWeight,
+    },
+    "md-bold": {
+      fontSize: title.md,
+      fontWeight: boldFontWeight,
+    },
+    sm: {
+      fontSize: title.sm,
+      fontWeight: normalFontWeight,
+    },
+  },
+
+  label: {
+    lg: {
+      fontSize: "1rem",
+      fontWeight: normalFontWeight,
+    },
+    md: {
+      fontSize: "0.85rem",
+      fontWeight: normalFontWeight,
+    },
+    sm: {
+      fontSize: "0.75em",
+      fontWeight: normalFontWeight,
+    },
+  },
+
+  body: {
+    lg: {
+      fontSize: "1.125rem",
+      fontWeight: normalFontWeight,
+    },
+    md: {
+      fontSize: "1rem",
+      fontWeight: normalFontWeight,
+    },
+    "md-bold": {
+      fontSize: "1rem",
+      fontWeight: boldFontWeight,
+    },
+    sm: {
+      fontSize: "0.85rem",
+      fontWeight: normalFontWeight,
+    },
+    xs: {
+      fontSize: "0.75rem",
+      fontWeight: normalFontWeight,
+    },
+  },
+
+  link: {
+    md: {
+      fontSize: "0.85rem",
+      fontWeight: normalFontWeight,
+    },
+    sm: {
+      fontSize: "0.75rem",
+      fontWeight: normalFontWeight,
+    },
+  },
+};
