fbcdab43eb63ec691720cf93cb2a83930afc42df
taniakoh <taniakohtzeern@gmail.com>
2025-12-09 08:26:56 +0800

    updated typography weights

--- src/theme/muiTheme.ts
+++ src/theme/muiTheme.ts
@@ -36,6 +36,24 @@ export const muiTheme = createTheme({
 
   typography: {
     fontFamily: "Inter, sans-serif",
+    h1:{
+      fontWeight:'600',
+    },
+    h2:{
+      fontWeight:'600',
+    },
+    h3:{
+      fontWeight:'600',
+    },
+    h4:{
+      fontWeight:'600',
+    },
+    h5:{
+      fontWeight:'600',
+    },
+    h6:{
+      fontWeight:'600',
+    }
   },
   components: {
     MuiTypography: {
