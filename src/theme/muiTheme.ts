7ffa0b531ac089bb815e87f80bdce92bf00ad8a2
taniakoh <taniakohtzeern@gmail.com>
2025-12-09 08:08:10 +0800

    updated with colour variants and typography

--- src/theme/muiTheme.ts
+++ src/theme/muiTheme.ts
@@ -1,21 +1,58 @@
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
+
 export const muiTheme = createTheme({
-    palette:{
-        primary:{
-            main:'#3959FF'
-        },
-        secondary:{
-            main:'#192873'
-        },
-        error:{
-            main:'#FF3939'
-        },
-        success:{
-            main:'#25BB5C'
-        }
+  palette: {
+    primary: {
+      main: "#3959FF",    // brand.primary
+      dark: "#192873",    // interaction.active (Hover states usually use dark)
+      light: "#879BFF",   // interaction.focus
+      contrastText: "#FFFFFF",
     },
-    typography:{
-        fontFamily:
-            'Inter, sans-serif',
-    }
+    secondary: {
+      main: "#192873",    // brand.secondary
+    },
+
+    text: {
+      primary: "#333333", 
+      secondary: "#707070",
+      disabled: "#CBCBCB", 
+    },
+    background: {
+      default: "#F5F5F5",   
+      paper: "#FFFFFF",     
+    },
+
+    error: {
+      main: "#FF3939",      // focus.error
+      dark:'#E20000',
+      light: "#FFEFEF",     // focus.errorHighlight (Good for Alert backgrounds)
+    },
+    success: {
+      main: "#79BD92",      // focus.success
+      dark:'#15843E',
+      light: "#9FF8BF",     // focus.successHighlight
+    },
+  },
+
+  typography: {
+    fontFamily: "Inter, sans-serif",
+  },
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          h1: 'h2',
+          h2: 'h2',
+          h3: 'h2',
+          h4: 'h2',
+          h5: 'h2',
+          h6: 'h2',
+          subtitle1: 'h2',
+          subtitle2: 'h2',
+          body1: 'span',
+          body2: 'span',
+        },
+      },
+    },
+  },
 });
