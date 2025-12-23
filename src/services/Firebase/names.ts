ddb5e8211f144f2d4ec2a8a56fe96d0e4336bcb8
karthik-Jay <32955712+karthik-Jay@users.noreply.github.com>
2025-12-23 08:17:40 +0800

    Mds 139 - Update consultant model (#183)

--- src/services/Firebase/names.ts
+++ src/services/Firebase/names.ts
@@ -1,7 +1,6 @@
 // define collections names used in firestore
 export class Collections {
   static readonly organisations = "organisations";
-  static readonly consultants = "consultants";
   static readonly users = "users";
   static readonly organisationsAdminData = "organisationAdminData";
   static readonly organisationPeopleSpotlight = "organisationPeopleSpotlight";
